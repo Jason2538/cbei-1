ad9ec6ed3c73c1037d276fc14dc65f4d698fb438
arolove2011 <133459416+arolove2011@users.noreply.github.com>
2023-05-13 04:45:11 +0000

    2563번

--- 2차원 배열/10798.py
+++ 2차원 배열/10798.py
@@ -1,9 +1,7 @@
-l = [input().split() for i in range(5)]
-
+l = [input() for i in range(5)]
 for x in range(15):
     for y in range(5):
         try:
             print(l[y][x], end="")
         except:
             pass
-        
