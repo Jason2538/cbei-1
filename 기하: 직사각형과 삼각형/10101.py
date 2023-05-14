d6fa32304389cf63d08251002c21e3966f7449a1
arolove2011 <133459416+arolove2011@users.noreply.github.com>
2023-05-14 09:47:15 +0000

    커밋 끝

---
+++ 기하: 직사각형과 삼각형/10101.py
@@ -0,0 +1,10 @@
+t = [int(input()) for _ in range(3)]
+
+if sum(t) != 180:
+    print("Error")
+elif t[0] == t[1] == t[2]:
+    print("Equilateral")
+elif t[0] != t[1] and t[0] != t[2] and t[1] != t[2]:
+    print("Scalene")
+else:
+    print("Isosceles")
